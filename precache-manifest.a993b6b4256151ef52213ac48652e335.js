dd441dfa7257b5022bc845308d9757cd2ef7355f
Bryan Wong <bryanwongzhicheng@gmail.com>
2020-06-11 20:57:59 -0400

    Updates

---
+++ precache-manifest.a993b6b4256151ef52213ac48652e335.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01387dd3c45d6c1bf2e00d3bcffe55f9",
+    "url": "/simple-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "2dd5f89e4156946663e1",
+    "url": "/simple-pomodoro-clock/static/css/main.7587ff68.chunk.css"
+  },
+  {
+    "revision": "e106763bd414c00422bb",
+    "url": "/simple-pomodoro-clock/static/js/2.3e9df188.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-pomodoro-clock/static/js/2.3e9df188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dd5f89e4156946663e1",
+    "url": "/simple-pomodoro-clock/static/js/main.33020be3.chunk.js"
+  },
+  {
+    "revision": "432cfdd2d079e782b8cf",
+    "url": "/simple-pomodoro-clock/static/js/runtime-main.af143560.js"
+  }
+]);
